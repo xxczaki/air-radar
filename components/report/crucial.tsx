96af5728393d87480f66f052bc78daffb4159183
Antoni Kepinski <a@kepinski.me>
2020-07-23 16:46:55 +0200

    Improve appearance, add tooltip with pollutant information, add measurement time

--- components/report/crucial.tsx
+++ components/report/crucial.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import {darken} from 'polished';
 import haversine from 'haversine';
+import Tooltip from 'rc-tooltip';
 import {Except} from 'type-fest';
 
 import ExtLink from '../extlink';
+import Description from './description';
 import {humanizeLevel} from '../../utils/humanize';
 import {Response} from '../../utils/fetcher';
 
@@ -21,7 +23,7 @@ const Wrapper = styled.section`
 const Box = styled.div<BoxProps>`
 	display: flex;
 	flex-direction: column;
-	justify-content: space-between;
+	justify-content: space-around;
 	border-radius: var(--radius);
 	background-color: ${props => props.background};
 	color: ${props => props.background === '#4caf50' || props.background === 'var(--gray)' ? 'var(--text)' : '#000'};
@@ -49,7 +51,7 @@ const Value = styled.div<BoxProps>`
 	width: 4rem;
 	height: 4rem;
 	padding: 1em;
-	transition: var(--transition-slow) all;
+	transition: all var(--transition-slow);
 	cursor: help;
 
 	h3, p {
@@ -74,16 +76,19 @@ const Crucial = ({coords, current, sensor}: Except<Response, 'forecast'>): JSX.E
 				<p>{current.indexes[0].description}</p>
 				<ValuesBox>
 					{current.values.map(element => (
-						<Value key={element.name} background={current.indexes[0].color}>
-							<p>{element.name}</p>
-							<h3>{element.value}</h3>
-						</Value>
+						<Tooltip key={element.name} placement="top" overlay={<Description name={element.name} value={element.value}/>}>
+							<Value background={current.indexes[0].color}>
+								<p>{element.name}</p>
+								<h3>{element.value}</h3>
+							</Value>
+						</Tooltip>
 					))}
 				</ValuesBox>
 			</Box>
 			<Box background="var(--gray)">
 				<h1>Sensor information</h1>
 				<p>Provider: <ExtLink href={sensor.provider === 'airly' ? 'https://map.airly.eu' : 'https://aqicn.org/'}><b>{sensor.provider === 'airly' ? 'Airly' : 'World Air Quality Index'}</b></ExtLink></p>
+				<p>Result from: <b>{current.time}</b></p>
 				<p>Distance: <b>{sensor.provider === 'airly' ? 'N/A' : `${Math.round((haversine(location, {latitude: sensor.latitude as number, longitude: sensor.longitude as number}) + Number.EPSILON) * 100) / 100} km`}</b></p>
 			</Box>
 		</Wrapper>
