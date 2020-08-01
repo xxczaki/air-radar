95227b43ef48c5e3160f7017fb7aa7aa47741eb3
Antoni Kepinski <a@kepinski.me>
2020-08-01 12:39:59 +0200

    use getStaticProps, fix URLs in development, add og image generation (wip)

--- pages_/reports/[id].tsx
+++ pages_/reports/[id].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NextPage, GetServerSideProps} from 'next';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import useTranslation from 'next-translate/useTranslation';
 import Skeleton from 'react-loading-skeleton';
 
 import Container from '../../components/container';
@@ -19,48 +20,62 @@ const OpenMap = dynamic(
 
 interface Props {
 	data: {
-		report: string;
+		report?: string;
 	};
 }
 
-export const getServerSideProps: GetServerSideProps = async ({query}) => {
-	const response = await fetch('https://air-radar.vercel.app/api/fetch', {
-		method: 'POST',
-		body: query.id as string
-	});
-	const data = await response.json();
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+	if (params?.id) {
+		const response = await fetch(`${process.env.NODE_ENV === 'production' ? 'https://air-radar.vercel.app' : 'http://localhost:3000'}/api/fetch`, {
+			method: 'POST',
+			body: params.id as string
+		});
+		const data = await response.json();
 
-	return {props: {data}};
+		return {props: {data}};
+	}
+
+	return {props: {data: null}};
 };
 
 const Index: NextPage<Props> = (props: Readonly<Props>) => {
 	const {data} = props;
-	const report: Response = JSON.parse(data.report)[0];
+
+	const {t} = useTranslation();
+
+	const report: Response = data?.report ? JSON.parse(data?.report)[0] : undefined;
+
+	console.log(report);
 
 	return (
 		<Container>
 			<Main>
-				<Head>
-					<link rel="preconnect" href="https://api.mapbox.com"/>
-					<link href="https://api.mapbox.com/mapbox-gl-js/v0.54.1/mapbox-gl.css" rel="stylesheet"/>
-				</Head>
-				{data ? (
-					<ReportContainer>
-						<OpenMap
-							location={{
-								latitude: report.coords.latitude,
-								longitude: report.coords.longitude
-							}}
-							sensor={{
-								latitude: report.sensor.latitude as number,
-								longitude: report.sensor.longitude as number
-							}}
-							color={report.current.indexes[0].color as string}
-						/>
-						<Report coords={report.coords} current={report.current} sensor={report.sensor}/>
-					</ReportContainer>
+				{report ? (
+					<>
+						<Head>
+							<link rel="preconnect" href="https://api.mapbox.com"/>
+							<link href="https://api.mapbox.com/mapbox-gl-js/v0.54.1/mapbox-gl.css" rel="stylesheet"/>
+							<title>{t('report:title', {id: report.id})}</title>
+							<meta property="og:image" content={`https://og.kepinski.me/${t('report:name')}.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fair-radar.vercel.app%2Fimages%2Fcloud-outline.svg`}/>
+							<meta name="twitter:image" content={`https://og.kepinski.me/${t('report:name')}.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fair-radar.vercel.app%2Fimages%2Fcloud-outline.svg`}/>
+						</Head>
+						<ReportContainer>
+							<OpenMap
+								location={{
+									latitude: report.coords.latitude,
+									longitude: report.coords.longitude
+								}}
+								sensor={{
+									latitude: report.sensor.latitude as number,
+									longitude: report.sensor.longitude as number
+								}}
+								color={report.current.indexes[0].color as string}
+							/>
+							<Report coords={report.coords} current={report.current} sensor={report.sensor}/>
+						</ReportContainer>
+					</>
 				) : (
-					<p>Loading...</p>
+					<h1>Report not found</h1>
 				)}
 			</Main>
 		</Container>
