d98b4eb133226e32b08474211c7a6b105f0faf61
Antoni Kepinski <a@kepinski.me>
2020-08-02 16:50:44 +0200

    update container

--- components/container.tsx
+++ components/container.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next-translate/Link';
 import styled from 'styled-components';
-import {SimpleImg} from 'react-simple-img';
 import useTranslation from 'next-translate/useTranslation';
 
 import Logo from './logo';
@@ -11,11 +10,10 @@ import Footer from './footer';
 import {usePreferences} from '../hooks/use-preferences';
 
 import cloudOutline from '../public/images/cloud-outline.svg';
-import email from '../public/images/email.svg';
-import logoGitHub from '../public/images/logo-github.svg';
 
 interface Props {
 	children: React.ReactNode;
+	reports?: number;
 }
 
 const Header = styled.header`
@@ -43,31 +41,7 @@ const Wrapper = styled.div`
 	justify-content: space-between;
 `;
 
-const Social = styled.div`
-	display: flex;
-	justify-content: space-between;
-	width: 12rem;
-`;
-
-const Box = styled.a`
-	align-items: center;
-	justify-content: center;
-	cursor: pointer;
-	transition: background-color var(--transition);
-	padding: 5px;
-	border-radius: var(--inline-radius);
-	user-select: none;
-
-	&:hover {
-		background-color: var(--hover);
-	}
-`;
-
-const Image = styled(SimpleImg)`
-	width: 2em;
-`;
-
-const Container = ({children}: Props): JSX.Element => {
+const Container = ({children, reports}: Props): JSX.Element => {
 	const {t} = useTranslation();
 	const {language} = usePreferences();
 
@@ -87,14 +61,7 @@ const Container = ({children}: Props): JSX.Element => {
 			{children}
 			<Footer>
 				<p>Antoni Kepinski &copy; {new Date().getFullYear()}</p>
-				<Social>
-					<Box href="mailto:a@kepinski.me" target="_blank" rel="noopener noreferrer" aria-label="Email">
-						<Image src={email} width="2em" height="2em" placeholder="var(--gray)" alt="Email"/>
-					</Box>
-					<Box href="https://github.com/xxczaki/air-radar" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-						<Image src={logoGitHub} width="2em" height="2em" placeholder="var(--gray)" alt="GitHub"/>
-					</Box>
-				</Social>
+				<p>Reports created: <b>{reports}</b></p>
 			</Footer>
 		</>
 	);
