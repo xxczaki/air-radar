477c2fad64fbc5530ef545e3ef027733d39065f3
Antoni Kepinski <a@kepinski.me>
2020-08-02 16:29:24 +0200

    Allow fetching all reports

--- pages_/api/fetch.ts
+++ pages_/api/fetch.ts
@@ -7,18 +7,20 @@ type Data = {
 };
 
 const fetchDocuments = async (request: NextApiRequest, response: NextApiResponse<Data | {message: string}>): Promise<void> => {
-	if (request.method === 'POST') {
-		try {
-			await client.connect();
+	try {
+		await client.connect();
 
-			const db = client.db(process.env.DB_NAME);
+		const db = client.db(process.env.DB_NAME);
 
+		if (request.method === 'POST') {
 			// MongoDB `find` !== Array.find
 			// eslint-disable-next-line unicorn/no-fn-reference-in-iterator
 			response.status(200).json({report: JSON.stringify(await db.collection(process.env.DB_COLLECTION ?? '').find({id: request.body}).toArray())});
-		} catch {
-			response.status(500).json({message: 'Failed fetching report. Please check the database status.'});
+		} else {
+			response.status(200).json({report: JSON.stringify(await db.collection(process.env.DB_COLLECTION ?? '').find().toArray())});
 		}
+	} catch {
+		response.status(500).json({message: 'Failed fetching report. Please check the database status.'});
 	}
 };
 
