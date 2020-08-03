d0946886cb5f8b891b9ab98078a03557a2a25666
Antoni Kepinski <a@kepinski.me>
2020-08-03 12:55:07 +0200

    Shrink size even more

--- components/report/crucial.tsx
+++ components/report/crucial.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import styled from 'styled-components';
 import {darken} from 'polished';
-// @ts-expect-error
-import LatLon from 'geodesy/latlon-vincenty.js';
-import {Tooltip} from 'react-tippy';
+import LatLon from 'geodesy/latlon-ellipsoidal-vincenty';
 import useTranslation from 'next-translate/useTranslation';
 import {Except} from 'type-fest';
 
@@ -13,6 +12,12 @@ import {humanizeLevel} from '../../utils/humanize';
 import {Response} from '../../utils/fetcher';
 import {usePreferences} from '../../hooks/use-preferences';
 import {convert} from '../../utils/convert';
+
+const Tooltip = dynamic(async () => {
+	const {Tooltip} = await import('react-tippy');
+
+	return Tooltip;
+});
 
 interface BoxProps {
 	background?: string;
@@ -27,7 +32,6 @@ const Wrapper = styled.section`
 const Box = styled.div<BoxProps>`
 	display: flex;
 	flex-direction: column;
-	justify-content: space-around;
 	border-radius: var(--radius);
 	background-color: ${props => props.background};
 	color: ${props => props.background === '#4caf50' || props.background === 'var(--gray)' ? 'var(--text)' : '#000'};
@@ -36,6 +40,10 @@ const Box = styled.div<BoxProps>`
 	h1 {
 		margin: 0;
 	}
+`;
+
+const InfoBox = styled.div`
+	justify-content: space-between;
 `;
 
 const ValuesBox = styled.div`
@@ -50,7 +58,7 @@ const Value = styled.div<BoxProps>`
 	flex-direction: column;
 	align-items: center;
 	justify-content: space-between;
-	background-color: ${props => darken(0.1, props.background as string)};
+	background-color: ${props => darken(0.125, props.background as string)};
 	border-radius: var(--inline-radius);
 	width: 4rem;
 	height: 4rem;
@@ -78,29 +86,33 @@ const Crucial = ({coords, current, sensor}: Except<Response, 'forecast' | 'id' |
 		<Wrapper>
 			<Box background={current.indexes[0].color}>
 				<h1>{sensor.provider === 'airly' ? humanizeLevel(current.indexes[0].level as string) : current.indexes[0].level}</h1>
-				<p>{current.indexes[0].description}</p>
-				<ValuesBox>
-					{current.values.map(element => (
-						<Tooltip
-							key={element.name}
-							interactive
-							arrow
-							position="top"
-							html={<Description name={element.name} value={element.value}/>}
-						>
-							<Value background={current.indexes[0].color}>
-								<p>{element.name}</p>
-								<h3>{element.value}</h3>
-							</Value>
-						</Tooltip>
-					))}
-				</ValuesBox>
+				<InfoBox>
+					<p>{current.indexes[0].description}</p>
+					<ValuesBox>
+						{current.values.map(element => (
+							<Tooltip
+								key={element.name}
+								interactive
+								arrow
+								position="top"
+								html={<Description name={element.name} value={element.value}/>}
+							>
+								<Value background={current.indexes[0].color}>
+									<p>{element.name}</p>
+									<h3>{element.value}</h3>
+								</Value>
+							</Tooltip>
+						))}
+					</ValuesBox>
+				</InfoBox>
 			</Box>
 			<Box background="var(--gray)">
 				<h1>{t('report:information')}</h1>
-				<p>{t('report:provider')} <ExtLink href={sensor.provider === 'airly' ? 'https://map.airly.eu' : 'https://aqicn.org/'}><b>{sensor.provider === 'airly' ? 'Airly' : 'World Air Quality Index'}</b></ExtLink></p>
-				<p>{t('report:from')} <b>{current.time}</b></p>
-				<p>{t('report:distance')} <b>{sensor.provider === 'airly' ? 'N/A' : convert(location.distanceTo(_sensor), unit)}</b></p>
+				<InfoBox>
+					<p>{t('report:provider')} <ExtLink href={sensor.provider === 'airly' ? 'https://map.airly.eu' : 'https://aqicn.org/'}><b>{sensor.provider === 'airly' ? 'Airly' : 'World Air Quality Index'}</b></ExtLink></p>
+					<p>{t('report:from')} <b>{current.time}</b></p>
+					<p>{t('report:distance')} <b>{sensor.provider === 'airly' ? 'N/A' : convert(location.distanceTo(_sensor), unit)}</b></p>
+				</InfoBox>
 			</Box>
 		</Wrapper>
 	);
