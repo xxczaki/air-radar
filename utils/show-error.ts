336bb9e3f0583d9ea645f92119c7e204ea70681d
Antoni Kepinski <a@kepinski.me>
2020-08-07 23:36:06 +0200

    fetch error

--- utils/show-error.ts
+++ utils/show-error.ts
@@ -3,27 +3,43 @@ import {toast} from 'react-toastify';
 interface Options {
 	createError: string;
 	locationError: string;
+	fetchError: string;
 }
 
-export const showError = async (type: 'create' | 'location', onError: () => void, {createError, locationError}: Options): Promise<void> => {
-	if (type === 'create') {
-		toast.error(createError, {
-			position: 'bottom-right',
-			autoClose: 2000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			progress: undefined
-		});
-	} else {
-		toast.error(locationError, {
-			position: 'bottom-right',
-			autoClose: 2000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			progress: undefined
-		});
+export const showError = async (type: 'create' | 'location' | 'fetch', onError: () => void, {createError, locationError, fetchError}: Options): Promise<void> => {
+	switch (type) {
+		case 'create':
+			toast.error(createError, {
+				position: 'bottom-right',
+				autoClose: 2500,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				progress: undefined
+			});
+			break;
+		case 'location':
+			toast.error(locationError, {
+				position: 'bottom-right',
+				autoClose: 2500,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				progress: undefined
+			});
+			break;
+		case 'fetch':
+			toast.error(fetchError, {
+				position: 'bottom-right',
+				autoClose: 2500,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				progress: undefined
+			});
+			break;
+		default:
+			break;
 	}
 
 	onError();
