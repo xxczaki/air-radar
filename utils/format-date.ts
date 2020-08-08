90aed532a0caf641d97ae51262e2690e3aa5671d
Antoni Kepinski <a@kepinski.me>
2020-08-08 22:17:15 +0200

    Fallback to date-fns

--- utils/format-date.ts
+++ utils/format-date.ts
@@ -2,6 +2,12 @@ export const formatDate = async (date: Date): Promise<string> => {
 	try {
 		const native = new Date(date).toLocaleTimeString('en', {hour: '2-digit', minute: '2-digit', hour12: false});
 
+		if (native === 'Invalid Date') {
+			const {lightFormat} = await import('date-fns');
+
+			return lightFormat(date, 'HH:mm');
+		}
+
 		return native;
 	} catch {
 		const {lightFormat} = await import('date-fns');
