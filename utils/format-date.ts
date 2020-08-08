ba644a5c3b17d4cc6d4e0049aed9628448b70073
Antoni Kepinski <a@kepinski.me>
2020-08-08 23:26:17 +0200

    use only date-fns

--- utils/format-date.ts
+++ utils/format-date.ts
@@ -1,18 +1,6 @@
+import {lightFormat} from 'date-fns';
+
 export const formatDate = async (date: Date): Promise<string> => {
-	try {
-		const native = new Date(date).toLocaleTimeString('en', {hour: '2-digit', minute: '2-digit', hour12: false});
-
-		if (native === 'Invalid Date') {
-			const {lightFormat} = await import('date-fns');
-
-			return lightFormat(date, 'HH:mm');
-		}
-
-		return native;
-	} catch {
-		const {lightFormat} = await import('date-fns');
-
-		return lightFormat(date, 'HH:mm');
-	}
+	return lightFormat(date, 'HH:mm');
 };
 
