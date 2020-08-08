ef30c623d33e5d8a9916066eeb88727036a1d2e1
Antoni Kepinski <a@kepinski.me>
2020-08-08 23:39:27 +0200

    final (hopefully) fix for iOS

--- utils/format-date.ts
+++ utils/format-date.ts
@@ -2,6 +2,12 @@ export const formatDate = async (date: string): Promise<string> => {
 	try {
 		const native = new Date(date).toLocaleTimeString('en', {hour: '2-digit', minute: '2-digit', hour12: false});
 
+		if (native === 'Invalid Date') {
+			const {parseISO, lightFormat} = await import('date-fns');
+
+			return lightFormat(parseISO(date), 'HH:mm');
+		}
+
 		return native;
 	} catch {
 		const {parseISO, lightFormat} = await import('date-fns');
