aff80d559a6ff1d3ae2965f771bc6dbfe7004c66
Antoni Kepinski <a@kepinski.me>
2020-08-08 23:00:51 +0200

    remove advice

--- utils/analyze-aqi.ts
+++ utils/analyze-aqi.ts
@@ -3,7 +3,6 @@
 interface AqiObject {
 	level: string;
 	description: string;
-	advice: string;
 	color: string;
 }
 
@@ -12,7 +11,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Good',
 			description: 'Air quality is considered satisfactory, and air pollution poses little or no risk.',
-			advice: 'None.',
 			color: '#4caf50'
 		};
 	}
@@ -21,7 +19,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Moderate',
 			description: 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.',
-			advice: 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.',
 			color: '#ffeb3b'
 		};
 	}
@@ -30,7 +27,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Unhealthy for sensitive groups',
 			description: 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.',
-			advice: 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.',
 			color: '#ffc107'
 		};
 	}
@@ -39,7 +35,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Unhealthy',
 			description: 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.',
-			advice: 'Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion.',
 			color: '#ff9800'
 		};
 	}
@@ -48,7 +43,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Very Unhealthy',
 			description: 'Health warnings of emergency conditions. The entire population is more likely to be affected.',
-			advice: 'Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion.',
 			color: '#ff5722'
 		};
 	}
@@ -57,7 +51,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 		return {
 			level: 'Hazardous',
 			description: 'Health alert: everyone may experience more serious health effects',
-			advice: 'Everyone should avoid all outdoor exertion.',
 			color: '#c41c00'
 		};
 	}
@@ -65,7 +58,6 @@ export const analyzeAqi = (aqi: number): AqiObject => {
 	return {
 		level: 'Unknown',
 		description: 'The air quality is unknown or is out of range.',
-		advice: 'None.',
 		color: '#424242'
 	};
 };
