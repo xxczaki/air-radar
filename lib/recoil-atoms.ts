055cb99c53912b49bb1c6828e0358b051052d6a0
Antoni Kepinski <a@kepinski.me>
2020-08-11 20:33:56 +0200

    Comments & Index atom

--- lib/recoil-atoms.ts
+++ lib/recoil-atoms.ts
@@ -1,15 +1,24 @@
 import {atom} from 'recoil';
 
+// Unit used when showing the distance between sensor and location
 export const _unit = atom<'km' | 'm' | 'mi'>({
 	key: 'unit',
 	default: 'km'
 });
 
+// Site language
 export const _language = atom<'en' | 'pl'>({
 	key: 'language',
 	default: 'en'
 });
 
+// Preferred air quality index
+export const _index = atom<'aqi-us' | 'aqhi' | 'daqi' | 'eaqi' | 'caqi'>({
+	key: 'index',
+	default: 'aqi-us'
+});
+
+// Reports created in this browser
 export const _reports = atom<Array<{id: string; key?: string}>>({
 	key: 'reports',
 	default: []
